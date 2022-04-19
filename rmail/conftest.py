c51dfa37b1ee3098f76e2c74f0d5eff591d56f73
Ron Royce <rroyce-dev@outlook.com>
2022-04-19 09:21:40 -0400

    Initial version

---
+++ rmail/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from rmail.users.models import User
+from rmail.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
